2e02e49e5534b487fe67c438ef034c32ecf4666a
Rwishav <grwishav34@gmail.com>
2021-05-25 04:51:29 +0530

    Rasa Chat Bot added. Alignments modified

---
+++ frontend/bot.js
@@ -0,0 +1,19 @@
+!(function () {
+    let e = document.createElement("script"),
+        t = document.head || document.getElementsByTagName("head")[0];
+    (e.src =
+        "https://cdn.jsdelivr.net/npm/rasa-webchat@1.0.0/lib/index.js"),
+        // Replace 1.x.x with the version that you want
+        (e.async = !0),
+        (e.onload = () => {
+            window.WebChat.default(
+                {
+                    customData: { language: "en" },
+                    socketUrl: "https://bf-botfront.development.agents.botfront.cloud",
+                    // add other props here
+                },
+                null
+            );
+        }),
+        t.insertBefore(e, t.firstChild);
+})();

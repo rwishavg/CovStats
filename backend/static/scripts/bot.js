1d7cbb90a642f91007cbc1b41161d133a7fec43c
rwishavg <grwishav34@gmail.com>
2021-10-19 02:08:12 +0530

    refactored and updated

---
+++ backend/static/scripts/bot.js
@@ -0,0 +1,23 @@
+!(function () {
+    let e = document.createElement("script"),
+        t = document.head || document.getElementsByTagName("head")[0];
+    (e.src =
+        "https://cdn.jsdelivr.net/npm/rasa-webchat@1.0.1/lib/index.js"),
+        // Replace 1.x.x with the version that you want
+        (e.async = !0),
+        (e.onload = () => {
+            window.WebChat.default(
+                {
+                    initPayload: '/greet',
+                    customData: { language: "en" },
+                    socketUrl: "http://localhost:5005",
+                    title: 'COVID19vsIndia StatAssistant',
+                    storage: "session"
+                    // add other props here
+
+                },
+                null
+            );
+        }),
+        t.insertBefore(e, t.firstChild);
+})();
